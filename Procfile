b6ff0e514b5b094368f4ac0341e28b8e83228f16
naoya25 <onaoya2002@gmail.com>
2023-11-27 05:11:06 +0900

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+ENV NLTK_DATA=/app/.heroku/nltk_data
 web: gunicorn app:app --log-file -
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
 from recommend import get_yani
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["GET"])
