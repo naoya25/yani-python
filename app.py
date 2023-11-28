f0d94601b3bd6694780a09a61b340dd75a4d7e46
naoya25 <onaoya2002@gmail.com>
2023-11-28 19:15:25 +0900

    fix リファクタ

--- app.py
+++ app.py
@@ -9,17 +9,23 @@ CORS(app)
 
 @app.route("/", methods=["GET"])
 def index():
-    return "flask running! ver 2.0"
+    return "Flask running! Version 2.0"
 
 
 @app.route("/api/recommend", methods=["POST"])
 def recommend():
     data = request.get_json()
-    post_data = data.get("postData")
+
+    if "postData" not in data:
+        return make_response(jsonify({"error": "Missing 'postData' in the request"}), 400)
+
+    post_data = data["postData"]
     recommendation = get_yani(post_data)
 
-    return make_response(jsonify({"recommendation": recommendation}))
+    response_data = {"recommendation": recommendation}
+    return jsonify(response_data)
 
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port)
--- recommend.py
+++ recommend.py
@@ -4,9 +4,14 @@ import scipy.spatial.distance
 import pandas as pd
 
 
-def calculate_words_vec(text):
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    model = BertModel.from_pretrained("bert-base-uncased")
+def load_yani_data(file_path="./static/yani-data.csv"):
+    return pd.read_csv(file_path, index_col=0)
+
+
+def tokenize_and_embed(text, model):
+    tokenizer = BertTokenizer.from_pretrained(model)
+    model = BertModel.from_pretrained(model)
+
     encoded_input = tokenizer(text, return_tensors="pt")
 
     with torch.no_grad():
@@ -14,22 +19,34 @@ def calculate_words_vec(text):
 
     embeddings = output.last_hidden_state[:, 0, :]
     vector = embeddings.tolist()[0]
+
     return vector
 
 
-def get_yani(post_data="タバコ最高"):
-    df = pd.read_csv("./static/yani-data.csv", index_col=0)
-    data_vec = calculate_words_vec(post_data)
+def calculate_similarity(vec1, vec2):
+    return 1 - scipy.spatial.distance.cosine(vec1, vec2)
+
+
+def find_most_similar_brand(post_data, df):
+    data_vec = tokenize_and_embed(post_data, "bert-base-uncased")
 
     max_brand, max_score = "", 0
     for i, feature in enumerate(df["特徴"]):
-        vec = calculate_words_vec(feature)
-        score = 1 - scipy.spatial.distance.cosine(data_vec, vec)
+        vec = tokenize_and_embed(feature, "bert-base-uncased")
+        score = calculate_similarity(data_vec, vec)
         if score > max_score:
             max_brand = df.loc[i, "銘柄"]
             max_score = score
+
     return {"brand": max_brand, "score": max_score}
 
 
+def get_yani(post_data="タバコ最高"):
+    df = load_yani_data()
+    result = find_most_similar_brand(post_data, df)
+    return result
+
+
 if __name__ == "__main__":
-    print(get_yani("タバコってなんか気づいたら吸っちゃってるんですよね。世界七不思議くらい不思議です"))
+    sample_text = "タバコってなんか気づいたら吸っちゃってるんですよね。世界七不思議くらい不思議です"
+    print(get_yani(sample_text))
